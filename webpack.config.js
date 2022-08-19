fd1aa79b076a96ace2bdf36f9d3e73deb03aec17
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-08-18 21:26:10 -0700

    Convert manifest file to v3 for chrome & opera

--- webpack.config.js
+++ webpack.config.js
@@ -60,6 +60,7 @@ module.exports = {
   entry: {
     manifest: path.join(sourcePath, 'manifest.json'),
     background: path.join(sourcePath, 'Background', 'index.ts'),
+    serviceworker: path.join(sourcePath, 'Background', 'service-worker.ts'),
     contentScript: path.join(sourcePath, 'ContentScript', 'index.ts'),
     popup: path.join(sourcePath, 'Popup', 'index.tsx'),
     options: path.join(sourcePath, 'Options', 'index.tsx'),
