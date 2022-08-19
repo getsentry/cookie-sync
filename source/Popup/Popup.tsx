7d7a1f9163e3515bd26fa6e44e8454f25d9ff22c
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-08-19 15:17:14 -0700

    v1 sync happens on popup click

--- source/Popup/Popup.tsx
+++ source/Popup/Popup.tsx
@@ -1,53 +1,156 @@
 import * as React from 'react';
-import {browser, Tabs} from 'webextension-polyfill-ts';
+import {browser, Cookies} from 'webextension-polyfill-ts';
 
 import './styles.scss';
 
-function openWebPage(url: string): Promise<Tabs.Tab> {
-  return browser.tabs.create({url});
+type Cookie = Cookies.Cookie;
+
+const SOURCE_URL: URL = new URL('https://sentry.io');
+const TARGET_URLS: URL[] = [
+  new URL('https://dev.getsentry.net'),
+  new URL('https://sentry.dev/'),
+];
+const COOKIE_NAME = 'session';
+
+function Error({e}: {e: Error | unknown}) {
+  return e ? (
+    <div style={{border: '1px solid red'}}>
+      <pre>{JSON.stringify(e, null, '\t')}</pre>
+    </div>
+  ) : null;
+}
+
+function Value({children, label}: {children: unknown; label: string}) {
+  return (
+    <React.Fragment>
+      <dt>{label}</dt>
+      <dd>
+        <pre>{JSON.stringify(children, null, '\t')}</pre>
+      </dd>
+    </React.Fragment>
+  );
+}
+
+async function genCurrentCookie(url: URL) {
+  return browser.cookies.get({
+    name: COOKIE_NAME,
+    url: url.href,
+  });
+}
+
+function useSourceCookie() {
+  const [error, setError] = React.useState<Error | unknown>();
+  const [sourceCookie, setSourceCookieValue] = React.useState<Cookie>();
+
+  const loadSource = React.useCallback(async () => {
+    try {
+      setSourceCookieValue(await genCurrentCookie(SOURCE_URL));
+    } catch (err) {
+      setError(err);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    loadSource();
+  }, [loadSource]);
+
+  return {
+    sourceCookie,
+    error,
+  };
+}
+
+function useTargetCookies() {
+  const [error, setError] = React.useState<Error | unknown>();
+  const [targetCookies, setTargetCookieValues] = React.useState<Cookie[]>();
+
+  const loadTargets = React.useCallback(async () => {
+    try {
+      setTargetCookieValues(
+        await Promise.all(TARGET_URLS.map(genCurrentCookie))
+      );
+    } catch (err) {
+      setError(err);
+    }
+  }, []);
+
+  const setTargets = React.useCallback(
+    async (cookie: undefined | Cookie) => {
+      if (!cookie) {
+        return;
+      }
+      try {
+        await Promise.all(
+          TARGET_URLS.map((url) => {
+            const details = {
+              url: url.href,
+              expirationDate: cookie.expirationDate,
+              httpOnly: cookie.httpOnly,
+              name: cookie.name,
+              sameSite: cookie.sameSite,
+              secure: cookie.secure,
+              value: cookie.value,
+            };
+            return browser.cookies.set(details);
+          })
+        );
+      } catch (err) {
+        setError(err);
+      }
+      loadTargets();
+    },
+    [loadTargets]
+  );
+
+  React.useEffect(() => {
+    loadTargets();
+  }, [loadTargets]);
+
+  return {
+    targetCookies,
+    setTargets,
+    error,
+  };
+}
+
+function useOnCookieChanged(callback: (cookie: Cookie) => void) {
+  React.useEffect(() => {
+    const onChanged = (changeInfo: Cookies.OnChangedChangeInfoType) => {
+      const {cookie} = changeInfo;
+      if (cookie.domain === SOURCE_URL.host && cookie.name === COOKIE_NAME) {
+        callback(changeInfo.cookie);
+      }
+    };
+    browser.cookies.onChanged.addListener(onChanged);
+
+    return () => {
+      browser.cookies.onChanged.removeListener(onChanged);
+    };
+  }, [callback]);
 }
 
 const Popup: React.FC = () => {
+  const {sourceCookie, error: sourceError} = useSourceCookie();
+  const {targetCookies, setTargets, error: targetError} = useTargetCookies();
+  React.useEffect(() => {
+    setTargets(sourceCookie);
+  }, [setTargets, sourceCookie]);
+
+  useOnCookieChanged(setTargets);
+
   return (
     <section id="popup">
-      <h2>WEB-EXTENSION-STARTER</h2>
-      <button
-        id="options__button"
-        type="button"
-        onClick={(): Promise<Tabs.Tab> => {
-          return openWebPage('options.html');
-        }}
-      >
-        Options Page
-      </button>
-      <div className="links__holder">
-        <ul>
-          <li>
-            <button
-              type="button"
-              onClick={(): Promise<Tabs.Tab> => {
-                return openWebPage(
-                  'https://github.com/abhijithvijayan/web-extension-starter'
-                );
-              }}
-            >
-              GitHub
-            </button>
-          </li>
-          <li>
-            <button
-              type="button"
-              onClick={(): Promise<Tabs.Tab> => {
-                return openWebPage(
-                  'https://www.buymeacoffee.com/abhijithvijayan'
-                );
-              }}
-            >
-              Buy Me A Coffee
-            </button>
-          </li>
-        </ul>
-      </div>
+      <h2>Cookie Sync</h2>
+      <Error e={sourceError} />
+      <Error e={targetError} />
+      <dl>
+        <Value label="Source URL">{SOURCE_URL}</Value>
+        <Value label="Target URLs">{TARGET_URLS}</Value>
+        <Value label="Cookie Name">{COOKIE_NAME}</Value>
+        <Value label="Cookie Value">{sourceCookie}</Value>
+
+        <Value label="Target Values">{targetCookies}</Value>
+      </dl>
     </section>
   );
 };
