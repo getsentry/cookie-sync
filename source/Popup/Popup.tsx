af004bbfb4aae204c66e963c64d355b5eaabe05e
Scott Cooper <scttcper@gmail.com>
2022-08-24 09:44:55 -0700

    feat: add styles to popup, fix service worker startup (#3)

--- source/Popup/Popup.tsx
+++ source/Popup/Popup.tsx
@@ -1,25 +1,60 @@
-import { browser } from "webextension-polyfill-ts";
+import { browser } from 'webextension-polyfill-ts';
 import * as React from 'react';
 
-import Value from './Value';
-
 import './styles.scss';
+import FailedIcon from './icons/FailedIcon';
+import SuccessIcon from './icons/SuccessIcon';
 
 const Popup: React.FC = () => {
-  const [lastResult, setLastResult] = React.useState();
+  const [result, setResult] = React.useState<any[] | string | null>(null);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   const syncNow = React.useCallback(async () => {
-    const resp = await browser.runtime.sendMessage({ command: "sync-now" });
-    setLastResult(resp);
+    setIsLoading(true);
+    setIsError(false);
+    setResult(null);
+
+    try {
+      const resp = await browser.runtime.sendMessage({ command: 'sync-now' });
+      setResult(resp);
+    } catch (err: any) {
+      setResult(err.message);
+      setIsError(true);
+      console.error(err);
+    } finally {
+      setIsLoading(false);
+    }
   }, []);
 
   return (
     <section id="popup">
-      <h2>Cookie Sync</h2>
-      <button type="button" onClick={() => syncNow()}>
-        Sync Cookies Now
-      </button>
-      {lastResult ? <Value label="Sync'd">{lastResult}</Value> : null}
+      <h1>Cookie Sync</h1>
+      <div style={{ textAlign: 'center' }}>
+        <button type="button" className="sync-button" onClick={syncNow} disabled={isLoading}>
+          Sync Cookies Now
+        </button>
+      </div>
+
+      {isError ? (
+        <div className="error-alert">
+          <FailedIcon width={20} height={20} />
+          <div>{result}</div>
+        </div>
+      ) : null}
+
+      {/* TODO: Assuming result is an array */}
+      {/* TODO: Could pull out into component */}
+      <div className="result">
+        {!isError && Array.isArray(result)
+          ? result.map((res, index) => (
+              <div key={index} className="success-row">
+                <SuccessIcon width={20} height={20} />
+                <div>{res.value.domain}</div>
+              </div>
+            ))
+          : null}
+      </div>
     </section>
   );
 };
