75638c5c5323056690424a939b3735a1943f21d3
Scott Cooper <scttcper@gmail.com>
2022-08-24 15:36:24 -0700

    fix: display *.sentry.dev correctly, remove unused files

--- source/Popup/Error.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export default function Error({e}: {e: Error | unknown}) {
-  return e ? (
-    <div style={{border: '1px solid red'}}>
-      <pre>{JSON.stringify(e, null, '\t')}</pre>
-    </div>
-  ) : null;
-}
