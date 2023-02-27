7b0b338b27cb18c7a6577ce2d62dfae00e17d106
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-02-27 11:14:04 -0800

    Setup Sentry

--- source/Popup/index.tsx
+++ source/Popup/index.tsx
@@ -1,6 +1,18 @@
+import { BrowserTracing } from "@sentry/tracing";
 import * as React from 'react';
+import * as Sentry from "@sentry/react";
+import packageJSON from "../../package.json";
+import Popup from "./Popup";
 import ReactDOM from 'react-dom';
 
-import Popup from './Popup';
+Sentry.init({
+  dsn: "https://a5b41882d81d4be6af9b19d7c081d4e4@o1.ingest.sentry.io/4504753500979200",
+  integrations: [new BrowserTracing()],
 
-ReactDOM.render(<Popup />, document.getElementById('popup-root'));
+  release: packageJSON.version,
+
+  sampleRate: 1.0,
+  tracesSampleRate: 1.0,
+});
+
+ReactDOM.render(<Popup />, document.getElementById("popup-root"));
