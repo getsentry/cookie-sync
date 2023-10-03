db850e70d23e321807889d0bdb9801fc228385d1
Scott Cooper <scttcper@gmail.com>
2023-10-03 15:55:54 -0700

    no sentry no emoji

--- source/Popup/Popup.tsx
+++ source/Popup/Popup.tsx
@@ -4,25 +4,19 @@ import DomainsEnabled from './DomainsEnabled';
 import FailedIcon from '../icons/FailedIcon';
 import ResultList from './ResultList';
 import useSyncNow from '../useSyncNow';
-import VersionBadge from './VersionBadge';
 
 import './popup.css';
 
 const Popup = () => {
-  const {results, isLoading, error, syncNow} = useSyncNow();
+  const { results, isLoading, error, syncNow } = useSyncNow();
 
   return (
     <section id="popup">
       <h1>Cookie Sync</h1>
 
       <DomainsEnabled />
-      <div style={{textAlign: 'center'}}>
-        <button
-          type="button"
-          className="sync-button"
-          onClick={syncNow}
-          disabled={isLoading}
-        >
+      <div style={{ textAlign: 'center' }}>
+        <button type="button" className="sync-button" onClick={syncNow} disabled={isLoading}>
           Sync Cookies Now
         </button>
       </div>
@@ -34,10 +28,7 @@ const Popup = () => {
         </div>
       ) : null}
 
-      <div className="result">
-        {results?.length ? <ResultList results={results} /> : null}
-      </div>
-      <VersionBadge />
+      <div className="result">{results?.length ? <ResultList results={results} /> : null}</div>
     </section>
   );
 };
--- source/Popup/index.tsx
+++ source/Popup/index.tsx
@@ -1,20 +1,7 @@
-import {BrowserTracing} from '@sentry/tracing';
 import * as React from 'react';
-import * as Sentry from '@sentry/react';
 import ReactDOM from 'react-dom';
 import ErrorBoundary from './ErrorBoundary';
-import packageJSON from '../../package.json';
 import Popup from './Popup';
-
-Sentry.init({
-  dsn: 'https://a5b41882d81d4be6af9b19d7c081d4e4@o1.ingest.sentry.io/4504753500979200',
-  integrations: [new BrowserTracing()],
-
-  release: packageJSON.version,
-
-  sampleRate: 1.0,
-  tracesSampleRate: 1.0,
-});
 
 ReactDOM.render(
   <ErrorBoundary>
